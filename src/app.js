b6b93f22bee84d18738529aecf864461fb4998da
Adarsh Gupta <111111623+adarsh-gupta-2002@users.noreply.github.com>
2025-03-03 18:50:37 +0530

    added home route

--- src/app.js
+++ src/app.js
@@ -11,4 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v3/app", eventRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 module.exports = app;
